c7328ec75dce06f7330eb746dbe170f7a65f0b57
Timothy Farkas <tim@simplifi.it>
2017-07-13 17:24:55 -0700

    - Added a create dag unit test

--- src/test/java/com/simplifi/it/rt/build/BuildConfigTest.java
+++ src/test/java/com/simplifi/it/rt/build/BuildConfigTest.java
@@ -2,6 +2,8 @@ package com.simplifi.it.rt.build;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import com.simplifi.it.rt.dag.DAG;
 import com.simplifi.it.rt.parse.ParseError;
 import com.simplifi.it.rt.parse.ParseResult;
 import junit.framework.Assert;
@@ -11,6 +13,7 @@ import org.junit.Test;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.simplifi.it.rt.build.BuildConfig.createRepoConfigMap;
 
@@ -60,6 +63,36 @@ public class BuildConfigTest
     Pair<Map<String, RepoConfig>, ParseError> mapPair = createRepoConfigMap(repoConfigs);
     Assert.assertNull(mapPair.getLeft());
     Assert.assertNotNull(mapPair.getRight());
+  }
+
+  @Test
+  public void simpleCreateRepoConfigDAGTest() {
+    Pair<DAG<RepoConfig>, ParseError> dagPair = BuildConfig.createRepoConfigDAG(createCorrectRepoConfigList());
+    DAG<RepoConfig> dag = dagPair.getLeft();
+    ParseError parseError = dagPair.getRight();
+
+    Assert.assertNull(parseError);
+
+    List<RepoConfig> repoConfigs = dag.getNodes().stream().collect(Collectors.toList());
+    Pair<Map<String, RepoConfig>, ParseError> actualMapPair = createRepoConfigMap(repoConfigs);
+    Map<String, RepoConfig> actualRepoConfigMap = actualMapPair.getLeft();
+    ParseError mapParseError = actualMapPair.getRight();
+    Assert.assertNull(mapParseError);
+    
+    Pair<Map<String, RepoConfig>, ParseError> expectedMapPair = createRepoConfigMap(createCorrectRepoConfigList());
+    Map<String, RepoConfig> expectedRepoConfigMap = expectedMapPair.getLeft();
+
+    Assert.assertEquals(expectedRepoConfigMap, actualRepoConfigMap);
+
+    RepoConfig repoConfig1 = actualRepoConfigMap.get("myRepo1");
+    RepoConfig repoConfig2 = actualRepoConfigMap.get("myRepo2");
+    RepoConfig repoConfig3 = actualRepoConfigMap.get("myRepo3");
+    RepoConfig repoConfig4 = actualRepoConfigMap.get("myRepo4");
+
+    Assert.assertEquals(Sets.newHashSet(), dag.getChildNodes(repoConfig1));
+    Assert.assertEquals(Sets.newHashSet(repoConfig1), dag.getChildNodes(repoConfig2));
+    Assert.assertEquals(Sets.newHashSet(repoConfig2, repoConfig4), dag.getChildNodes(repoConfig3));
+    Assert.assertEquals(Sets.newHashSet(repoConfig1), dag.getChildNodes(repoConfig4));
   }
 
   private List<RepoConfig> createCorrectRepoConfigList() {
